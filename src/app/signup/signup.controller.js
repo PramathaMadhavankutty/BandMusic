7d7fc997785f478a3ded0a4c957636e378c0e601
PramathaMAdhavankutty <pramatha21992@gmail.com>
2016-03-17 12:47:28 +0000

    save all changes

--- src/app/signup/signup.controller.js
+++ src/app/signup/signup.controller.js
@@ -6,11 +6,23 @@
     .controller('SignUpController', SignUpController);
 
   /** @ngInject */
-  function SignUpController($log,$location) {
+  function SignUpController($log, $location) {
     var vm = this;
-    vm.user ={};
+    vm.user = {};
     vm.wasSubmitted = false;
     vm.submit = function(submit) {
+<<<<<<< HEAD
+      if (submit.$valid) {
+        vm.wasSubmitted = true;
+        $location.path('/login');
+      }
+      else {
+        $log('form is invalid');
+        vm.wasSubmitted = true;
+      }
+
+    };
+=======
                 if(submit.$valid){
                   vm.wasSubmitted=true;
                   $location.path('/login/'); 
@@ -24,5 +36,6 @@
                 $location.path('/'); 
    };
    
+>>>>>>> 504c960e44a271af768c063e35773d42bc83bd89
   }
 })();
