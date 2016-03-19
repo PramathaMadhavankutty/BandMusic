e23b9988fb87ea055fbf27107d5da2ac1a4c3185
PramathaMAdhavankutty <pramatha21992@gmail.com>
2016-03-19 00:20:57 +0000

    modify gigs

--- src/app/gigs/gigs.controller.js
+++ src/app/gigs/gigs.controller.js
@@ -6,9 +6,39 @@
     .controller('GigsController', GigsController);
 
   /** @ngInject */
-  function GigsController() {
+  function GigsController($http) {
     var vm = this;
-
-    vm.title = 'Gigs';
-  }
+    vm.options = {
+    rowHeight: 50,
+    headerHeight: 50,
+    footerHeight: false,
+    scrollbarV: false,
+    selectable: false,
+    columns: [
+    {
+      name: "Date",
+      
+    }, 
+    {
+      name: "Venue"
+    }, 
+    {
+      name: "Location"
+    },
+    {
+      name: "Tickets"
+    }
+    ]
+  };
+    vm.data=[];
+     function loadDataTable(){
+      $http.get('/table.json')
+        .then(function(response){
+          vm.data = response.data;
+        });
+    }
+    
+    loadDataTable();
+}
+  
 })();
